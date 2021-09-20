99610ea4b5c0486ae34228216e7b1582f42af24b
Lily Liu <lily.liu@mths.ca>
2021-09-20 16:31:23 +0000

    added random guessing

---
+++ random_guessing.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+# Created by: Lily Liu
+# Created on: Sept 2021
+# This lets user guess a number
+# Program will say right or wrong and give answer
+
+import random
+
+
+def main():
+    # This lets user guess a number
+
+    # input
+    guess_number = int(input("Enter a number between 1-9 : "))
+    some_variable = random.randint(1, 9)  # a number between 1 and 9
+
+    # process and output
+    if guess_number == some_variable:
+        print("You guessed correctly!")
+    else:
+        print("You guessed wrong!")
+    print("The correct answer is {0}".format(some_variable))
+
+    print("")
+    print("Done.")
+
+
+if __name__ == "__main__":
+    main()
